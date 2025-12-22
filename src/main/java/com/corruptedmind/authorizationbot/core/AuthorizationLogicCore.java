7f355770f611ba91deafdc1d21ee5a9d76217aca
admin <ilya_seredkiniam@mail.ru>
2025-12-23 01:12:33 +0500

    test

--- src/main/java/com/corruptedmind/authorizationbot/core/AuthorizationLogicCore.java
+++ src/main/java/com/corruptedmind/authorizationbot/core/AuthorizationLogicCore.java
@@ -5,6 +5,7 @@ import com.corruptedmind.authorizationbot.model.UserResponse;
 
 public class AuthorizationLogicCore implements LogicCore {
 
+
     @Override
     public UserResponse handle(UserRequest userRequest) {
         return new UserResponse(userRequest.text());
