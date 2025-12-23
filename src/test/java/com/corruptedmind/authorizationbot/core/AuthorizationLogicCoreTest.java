6054434a312568c0bb362c91b3dbcff8d8ea553a
admin <ilya_seredkiniam@mail.ru>
2025-12-23 15:45:27 +0500

    test

--- src/test/java/com/corruptedmind/authorizationbot/core/AuthorizationLogicCoreTest.java
+++ src/test/java/com/corruptedmind/authorizationbot/core/AuthorizationLogicCoreTest.java
@@ -13,6 +13,6 @@ class AuthorizationLogicCoreTest {
 
         AuthorizationLogicCore logicCore = new AuthorizationLogicCore();
         String result = logicCore.handle(new UserRequest(testMessage)).text();
-        assertEquals(testMessage, result);
+        assertEquals("now", result);
     }
 }
